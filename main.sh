88718205461670e1c3a9c5a9b355c084b4965bdb
Joshuapmb <43140782+Joshuapmb@users.noreply.github.com>
2019-10-03 12:55:48 +0100

    Adding start of menu

--- main.sh
+++ main.sh
@@ -1,1 +1,22 @@
 #!/usr/bin/env bash
+
+//Menu
+PS3='Please enter your choice: '
+options=("Option 1" "Option 2" "Quit")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "1) Open a File Repository")
+            echo "Option 1 selected, open a file repository"
+            break
+            ;;
+        "2) Create a New File Repository")
+            echo "Option 2 selected, create a new file repository"
+            break
+            ;;
+        "Quit")
+            break
+            ;;
+        *) echo "invalid option $REPLY";;
+    esac
+done
