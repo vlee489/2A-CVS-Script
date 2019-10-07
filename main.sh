882a19f077902f723c83655d72f5ca2fc019bb8e
JesseChan <jessechan@linux042.computing.dundee.ac.uk>
2019-10-07 15:58:19 +0100

    One error needs fix

--- main.sh
+++ main.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-file=NULL; 
+file="NULL"; 
 back=false;
 option=null;
 ##Make a menu ask whether they want to edit,create,delete or exit text file 
@@ -16,28 +16,27 @@ read -p "Please enter an option: " option
 case $option in
   1 ) echo "You entered one, open a file repository"
     ls
-    while !back do:
+   ## while !back do:
     echo "What file would you like to edit"
-    read $file
-    if [ -f $file ]; then
+    read file
+    if [ -f $file ] ;then
 
-	   chmod u+w $file; # Only changes user i can't seem  to change the other people permissions
+	   chmod u+w "$file" # Only changes user i can't seem  to change the other people permissions
 	    vi $file ## or allow user to type stuff cat show and then sed "text"
 	    ## cp current location to backup location ## Back up ????? create copy and move to .backup ???  
-	     chmod u-w ## Needs checking
-         chmod u+r ## Change it back to read only chmod 222 or the 0ther ones
-	    // Log changes in a log file DUnno ??????
-	    back=true;
-
+	     chmod u-w "$file" ## Needs checking
+       chmod u+r "$file" ## Change it back to read only chmod 222 or the 0ther ones
+	    ##// Log changes in a log file DUnno ??????
+	    ##back=true;
     else
    	    echo "The file '$file' in not found"
     fi
 
-if[ $file= "0" ]; then
+if [ $file == "0"] ;then
     back=true;
-fi
-;done
-  ;;
+fi 
+##;done
+ ;;
   2 ) echo "You entered two, What would you like to call the file:"        
         read file 
         touch $file 
